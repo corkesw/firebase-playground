a40b80f834375e3dbacbe10509d872a2e2f0770e
corkesw <home@stephencorke.com>
2021-10-28 14:14:17 +0100

    remove duplicate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { getDatabase, ref, onValue } from "firebase/database";
+import { initializeApp } from "@firebase/app";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBtqgv2T2j-H7JOdeSdqoPf0Qn48yfGhss",
+  authDomain: "doggo-71b8a.firebaseapp.com",
+  databaseURL:
+    "https://doggo-71b8a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "doggo-71b8a",
+  storageBucket: "doggo-71b8a.appspot.com",
+  messagingSenderId: "481919035597",
+  appId: "1:481919035597:web:c2c132022df06ba639a184",
+  measurementId: "G-5E447XMNRX",
+});
+
+const db = getDatabase();
+
 
 function App() {
+
+  const [dogs, setDogs] = useState([])
+  useEffect(() => {
+
+    const dogRef = ref(db, "dogs/");
+
+    onValue(dogRef, (snapshot) => {
+      const data = snapshot.val();
+      const dogNames = Object.keys(data);
+      setDogs(dogNames)
+    });
+    
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {dogs.map((dog) => {
+          return <li key={dog}>{dog}</li>;
+        })}
+      </ul>
     </div>
   );
 }
